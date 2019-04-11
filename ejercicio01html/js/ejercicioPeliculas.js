107e4a7ac5b0ae286f3c51edb68c3049748d28a3
jrrossell <junior.rossell@gmail.com>
2019-04-11 17:52:19 +0200

    Continuacion ejercicio Peliculas

--- ejercicio01html/js/ejercicioPeliculas.js
+++ ejercicio01html/js/ejercicioPeliculas.js
@@ -1,5 +1,81 @@
 var i = 1
 
+function inicializar() {
+    if (localStorage.getItem("id") <= 1) {
+        var ajax = new XMLHttpRequest();
+        ajax.open("GET", "data.json");
+        ajax.onreadystatechange = function () {
+            if (ajax.status == 200 && ajax.readyState == 4) {
+                datos = JSON.parse(ajax.responseText)
+                console.log(datos);
+
+                var titulo = datos[0].titulo
+                var director = datos[0].director
+                var cod = datos[0].cod
+                var genero = datos[0].genero
+                var fecha = datos[0].fecha
+                var valoracion = datos[0].valoracion
+
+                localStorage.setItem("titulo" + i, titulo)
+                localStorage.setItem("director" + i, director)
+                localStorage.setItem("cod" + i, cod)
+                localStorage.setItem("genero" + i, genero)
+                localStorage.setItem("fecha" + i, fecha)
+                localStorage.setItem("valoracion" + i, valoracion)
+
+                var trPelicula = '<tr>'
+                    + '<td>' + cod + '</td>'
+                    + '<td>' + titulo + '</td>'
+                    + '<td>' + director + '</td>'
+                    + '<td>' + genero + '</td>'
+                    + '<td>' + fecha + '</td>'
+                    + '<td>' + valoracion + '</td>'
+                    + '<td><button onclick="eliminar(' + i + ')">Eliminar</button>'
+                    + '<button onclick="editar(' + i + ')">Editar</button></td>'
+                    + '</tr>'
+
+                var tbody = document.getElementById("filaDePeliculas")
+                var tr = document.createElement("tr")
+                tr.id = i
+                tr.innerHTML = trPelicula
+                tbody.appendChild(tr)
+
+                localStorage.setItem("id", i)
+                i++
+            }
+        }
+        ajax.send();
+    } else {
+        for (var j = 1; j <= localStorage.getItem("id"); j++) {
+
+            var titulo = localStorage.getItem("titulo" + j)
+            var director = localStorage.getItem("director" + j)
+            var cod = localStorage.getItem("cod" + j)
+            var genero = localStorage.getItem("genero" + j)
+            var fecha = localStorage.getItem("fecha" + j)
+            var valoracion = localStorage.getItem("valoracion" + j)
+
+            var trPelicula = '<tr>'
+                + '<td>' + cod + '</td>'
+                + '<td>' + titulo + '</td>'
+                + '<td>' + director + '</td>'
+                + '<td>' + genero + '</td>'
+                + '<td>' + fecha + '</td>'
+                + '<td>' + valoracion + '</td>'
+                + '<td><button onclick="eliminar(' + j + ')">Eliminar</button>'
+                + '<button onclick="editar(' + j + ')">Editar</button></td>'
+                + '</tr>'
+
+            var tbody = document.getElementById("filaDePeliculas")
+            var tr = document.createElement("tr")
+            tr.id = j
+            tr.innerHTML = trPelicula
+            tbody.appendChild(tr)
+        }
+        i = j
+    }
+}
+
 function agregar() {
     var titulo = document.getElementById("titulo").value
     var director = document.getElementById("director").value
@@ -8,44 +84,62 @@ function agregar() {
     var fecha = document.getElementById("fecha").value
     var valoracion = document.getElementById("valoracion").value
 
-    //var tr = '<tr><td>' + cod + '</td><td>' + titulo + '</td><td>' + director + '</td><td>' + genero + '</td><td>' + fecha + '</td><td>' + valoracion + '</td><td><button>Editar</button><button>Eliminar</button></td></tr>'
-   
-    var tbody = document.getElementById("valor")
-    
+    localStorage.setItem("titulo" + i, titulo)
+    localStorage.setItem("director" + i, director)
+    localStorage.setItem("cod" + i, cod)
+    localStorage.setItem("genero" + i, genero)
+    localStorage.setItem("fecha" + i, fecha)
+    localStorage.setItem("valoracion" + i, valoracion)
+
+    var trPelicula = '<tr>'
+        + '<td>' + cod + '</td>'
+        + '<td>' + titulo + '</td>'
+        + '<td>' + director + '</td>'
+        + '<td>' + genero + '</td>'
+        + '<td>' + fecha + '</td>'
+        + '<td>' + valoracion + '</td>'
+        + '<td><button onclick="eliminar(' + i + ')">Eliminar</button>'
+        + '<button onclick="editar(' + i + ')">Editar</button></td>'
+        + '</tr>'
+
+    var tbody = document.getElementById("filaDePeliculas")
     var tr = document.createElement("tr")
-
-    var td0 = document.createElement("td")
-    td0.id = i
-    console.log(td0.id)
-    var td1 = document.createElement("td")
-    var td2 = document.createElement("td")
-    var td3 = document.createElement("td")
-    var td4 = document.createElement("td")
-    var td5 = document.createElement("td")
-    var td6 = document.createElement("td")
-
-    td0.innerHTML = cod
-    td1.innerHTML = titulo
-    td2.innerHTML = director         
-    td3.innerHTML = genero
-    td4.innerHTML = fecha
-    td5.innerHTML = valoracion
-    td6.innerHTML = '<button onclick="eliminar(' + i + ')">Eliminar</button>'
-
-    tr.appendChild(td0)
-    tr.appendChild(td1)
-    tr.appendChild(td2)
-    tr.appendChild(td3)
-    tr.appendChild(td4)
-    tr.appendChild(td5)
-    tr.appendChild(td6)
-
+    tr.id = i
+    tr.innerHTML = trPelicula
     tbody.appendChild(tr)
+
+    localStorage.setItem("id", i)
     i++
+    limpiarCampos()
 }
 
-function eliminar(row){
-    var eliminar = document.getElementById(row).id
-    console.log(eliminar)
-    document.getElementById("mitable").deleteRow(eliminar);
+function eliminar(j) {
+    document.getElementById(j).remove()
+    localStorage.removeItem(j)
+}
+
+function limpiarCampos() {
+    document.getElementById("titulo").value = ""
+    document.getElementById("director").value = ""
+    document.getElementById("cod").value = ""
+    document.getElementById("genero").value = ""
+    document.getElementById("fecha").value = ""
+    document.getElementById("valoracion").value = ""
+}
+
+function editar(i) {
+    document.getElementById("titulo").value = localStorage.getItem("titulo" + i)
+    document.getElementById("director").value = localStorage.getItem("director" + i)
+    document.getElementById("cod").value = localStorage.getItem("cod" + i)
+    document.getElementById("genero").value = localStorage.getItem("genero" + i)
+    document.getElementById("fecha").value = localStorage.getItem("fecha" + i)
+    document.getElementById("valoracion").value = localStorage.getItem("valoracion" + i)
+
+    document.getElementById(i).remove()
+
+}
+
+function vaciarTabla() {
+    localStorage.removeItem("id")
+    location.reload()
 }
